2ac57816f7b79020f3fc9a41817e86d6330a5c21
Joey Ashcroft <ashcroft.joey@gmail.com>
2019-05-01 01:51:18 -0700

    drop down menus

--- static/js/app.js
+++ static/js/app.js
@@ -18,6 +18,7 @@ var tableData = data;
 
 var tbody  = d3.select("tbody")
 
+
 //function to create table using data object as input 
 function create_table(data){
   data.forEach(function(d){
@@ -39,13 +40,55 @@ function create_table(data){
 //create table with all data
 create_table(tableData)
 
+
+
 //button filter
-var button = d3.select("#filter-btn")
-var input_date = d3.select("#datetime")
-var input_city = d3.select("#city")
-var input_state = d3.select("#state")
-var input_country = d3.select("#country")
-var input_shape = d3.select("#shape")
+var button = d3.select("#filter-btn");
+var input_date = d3.select("#datetime");
+var input_city = d3.select("#city");
+var input_state = d3.select("#state");
+var input_country = d3.select("#country");
+var input_shape = d3.select("#shape");
+
+//********CREATE DROP DOWN MENU FOR EACH VARIABLE*****
+var datetime=[];
+var city=[];
+var state=[];
+var country=[];
+var shape=[];
+
+function dropdown_options(data){
+Object.entries(data).forEach(function([key,value]){
+//push values to array  
+datetime.push(value["datetime"])
+city.push(value["city"])
+state.push(value["state"])
+country.push(value["country"])
+shape.push(value["shape"])
+});
+
+//const to get unique values
+const unique = (value, index, self) => {
+  return self.indexOf(value) === index
+}
+
+//rewrite variable with only unique values
+datetime = datetime.filter(unique)
+city = city.filter(unique)
+state = state.filter(unique)
+country = country.filter(unique)
+shape = shape.filter(unique)
+
+//push values to drop down menu
+datetime.map(i => d3.select("#datetime").append("option").text(i))
+city.map(i => d3.select("#city").append("option").text(i))
+state.map(i => d3.select("#state").append("option").text(i))
+country.map(i => d3.select("#country").append("option").text(i))
+shape.map(i => d3.select("#shape").append("option").text(i))
+
+};
+
+dropdown_options(data)
 
 //function that outputs new filtered data by input date
 function handleChange(){
@@ -58,6 +101,13 @@ function handleChange(){
 
   //clear previous table
   tbody.text("")
+
+  //TEST
+  if (input_date.property("value")){
+  var date = input_date.property("value");
+  //create new data object with filtered rows by input date
+  filtered = data.filter(i => i.datetime == date);
+  };
 
   //if a value is input for date filter the data
   if (input_date.property("value")){
@@ -94,9 +144,13 @@ function handleChange(){
   filtered = filtered.filter(i => i.shape == shape);
   };
 
+  //refresh dropdown options
+  dropdown_options(filtered)
+
 
   //input this new data object into our function to output the table
   create_table(filtered)
+
 }
 
 
